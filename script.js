742af5bf2e300eb71e2e6d78469ffa3a706403cb
pramuditopr <134248499+pramuditopr@users.noreply.github.com>
2025-06-14 10:52:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+
+menuToggle.addEventListener('click', () => {  navList.classList.toggle('active');});
+
+
+AOS.init({
+  once: true
+});;
